27ae33c41a662b1d0604c225d90af66644f0ab2c
Lan <xuyh0120@outlook.com>
2018-10-22 14:31:03 -0500

    Add comment to vga_text.c

--- student-distrib/vga_text.c
+++ student-distrib/vga_text.c
@@ -1,10 +1,25 @@
 #include "vga_text.h"
 
+/* void vga_text_set_color(uint8_t x, uint8_t y, uint8_t foreground, uint8_t background)
+ * @input: x, y - coordinate of character whose color to be changed.
+ *         foreground - foreground color to be changed, using VGA text mode palette.
+ *         background - background color to be changed, using VGA text mode palette.
+ * @output: the character at (x, y) has color changed to *foreground* and *background*
+ * @description: change the color of character at (x, y) in VGA text mode.
+ *               Initially used to draw Aqua on exception screen, but can be used for others.
+ */
 void vga_text_set_color(uint8_t x, uint8_t y, uint8_t foreground, uint8_t background) {
     uint8_t color = (background & 0xF) << 4 | (foreground & 0xF);
     *(uint8_t*) (VIDEO + (y * SCREEN_WIDTH + x) * 2 + 1) = color;
 }
 
+/* void vga_text_set_character(uint8_t x, uint8_t y, uint8_t ch)
+ * @input: x, y - coordinate of character whose color to be changed.
+ *         ch - character to be set at (x, y).
+ * @output: the character at (x, y) is changed to *ch*
+ * @description: change the character at (x, y) in VGA text mode.
+ *               Initially used to draw Aqua on exception screen, but can be used for others.
+ */
 void vga_text_set_character(uint8_t x, uint8_t y, uint8_t ch) {
     *(uint8_t*) (VIDEO + (y * SCREEN_WIDTH + x) * 2) = ch;
 }
