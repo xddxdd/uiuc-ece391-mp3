602427b958782ae1e0228af03b41c950162d9bae
Lan <xuyh0120@outlook.com>
2018-10-22 00:17:55 -0500

    Implement keyboard scancode to ascii conversion

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -145,7 +145,7 @@ void entry(unsigned long magic, unsigned long addr) {
     /* Initialize devices, memory, filesystem, enable device interrupts on the
      * PIC, any other initialization stuff... */
     keyboard_init();
-    rtc_init();
+    //rtc_init();
 
     /* Enable interrupts */
     /* Do not enable the following until after you have set up your
@@ -154,7 +154,9 @@ void entry(unsigned long magic, unsigned long addr) {
     printf("Enabling Interrupts\n");
     sti();
 
-    rtc_set_freq(2);    // Set frequency after initialization,
+    clear();
+
+    //rtc_set_freq(2);    // Set frequency after initialization,
                         // because it includes CLI and STI
 
 #ifdef RUN_TESTS
--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,4 +1,5 @@
 #include "keyboard.h"
+#include "keyboard-scancode.h"
 
 /* void keyboard_init()
  * @effects: Make the system ready to receive keyboard interrupts
@@ -15,7 +16,9 @@ void keyboard_init() {
 void keyboard_interrupt() {
     uint8_t key = inb(KEYBOARD_PORT);
 
-    printf("Received scancode %d\n", key);
+    if(key < SCANCODE_TABLE_SIZE) {
+        putc(scancode[key][0]);
+    }
 
     send_eoi(KEYBOARD_IRQ);
 }
