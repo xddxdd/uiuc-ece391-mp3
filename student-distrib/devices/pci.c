9cdfe56286f281d87f7da4b68fc273740452e554
Lan <xuyh0120@outlook.com>
2018-12-07 15:17:09 -0600

    Smarter PCI scanning

--- student-distrib/devices/pci.c
+++ student-distrib/devices/pci.c
@@ -1,28 +1,62 @@
 #include "pci.h"
 #include "qemu_vga.h"
 
+/* int32_t pci_get_device(uint8_t bus, uint8_t device, uint8_t func, pci_device_t* ret)
+ * @input: bus, device, func - index of the PCI slot
+ *         ret - where to write the return value to
+ * @output: ret val - SUCCESS / FAIL
+ *          ret - written with description for this PCI device
+ * @description: probes if there is PCI device connected to the slot, and if there is any,
+ *     get its 64-to-72-byte information and write it into the return value.
+ *     C doesn't support returning a 64-byte value on stack, so pointer is used.
+ */
 int32_t pci_get_device(uint8_t bus, uint8_t device, uint8_t func, pci_device_t* ret) {
-    if(NULL == ret) return FAIL;
+    if(bus >= PCI_COUNT_BUS
+        || device >= PCI_COUNT_DEVICE
+        || func >= PCI_COUNT_FUNC
+        || ret == NULL) return FAIL;
+    // Compose the address for the query
     pci_addr_t addr = {0};
     addr.func_num = func;
     addr.device_num = device;
     addr.bus_num = bus;
     addr.enable = 1;
+    // Register upper bound, 0x10 for standard devices, 0x12 for CardBus
+    int reg_bound = 0x10;
+    // Get each of the 16 segments of the 64-byte information
     for(addr.reg_num = 0; addr.reg_num < 0x10; addr.reg_num++) {
         outl(addr.val, PCI_REG_ADDR);
         ret->val[addr.reg_num] = inl(PCI_REG_DATA);
+        // If this device doesn't exist (vendor_id and device_id are both 0xffff),
+        // return FAIL.
         if(addr.reg_num == 0 && (ret->vendor_id == 0xffff || ret->device_id == 0xffff)) {
             return FAIL;
+        }
+        if(addr.reg_num == 3 && ret->header_type == PCI_TYPE_CARDBUS) {
+            reg_bound = 0x12;
         }
 
     }
     return SUCCESS;
 }
 
+/* void pci_register_device(uint8_t bus, uint8_t device, uint8_t func, pci_device_t* dev_info)
+ * @input: bus, device, func - index of the PCI slot
+ *         dev_info - pointer to the description of the device
+ * @output: if the device is recognized, then some drivers are configured according to PCI info
+ * @description: configures some devices based on PCI info.
+ *     currently only configures QEMU VGA device.
+ */
 void pci_register_device(uint8_t bus, uint8_t device, uint8_t func, pci_device_t* dev_info) {
+    if(bus >= PCI_COUNT_BUS
+        || device >= PCI_COUNT_DEVICE
+        || func >= PCI_COUNT_FUNC
+        || dev_info == NULL) return;
+    // Device specific handler
     switch((uint32_t) dev_info->vendor_id << 16 | dev_info->device_id) {
-        case 0x12341111:
-            qemu_vga_addr = dev_info->bar[0];
+        case 0x12341111:    // vendor device id pair for QEMU VGA device
+            // Register bar0 address as the address for linear buffer
+            qemu_vga_addr = dev_info->device.bar[0];
             if(qemu_vga_addr & 1) {
                 // IO space BAR
                 qemu_vga_addr &= PCI_MASK_BAR_IOSPACE;
@@ -35,15 +69,36 @@ void pci_register_device(uint8_t bus, uint8_t device, uint8_t func, pci_device_t
     }
 }
 
+/* void pci_init()
+ * @output: PCI devices get scanned and initialized
+ * @description: scans every slot of the PCI bus, detects and initializes everything.
+ */
 void pci_init() {
     uint8_t bus, device, func;
     pci_device_t dev_info;
+
+    // 32 * 8 = 256 bits, marks the presence of all PCI buses.
+    // PCI bus num = array index * 8 + bit index
+    uint8_t buses[32] = {0};
+    buses[0] = 1;   // Mark bus 0 as present
+
+    // Smart scan of the whole PCI bus.
+    // Start with bus 0, and if a PCI bridge is detected, add that bridge into list.
     for(bus = 0; bus < PCI_COUNT_BUS; bus++) {
+        if(!(buses[bus / 8] & (1 << (bus % 8)))) continue;
+        printf("PCI scan on bus %d\n", bus);
         for(device = 0; device < PCI_COUNT_DEVICE; device++) {
             for(func = 0; func < PCI_COUNT_FUNC; func++) {
                 if(FAIL == pci_get_device(bus, device, func, &dev_info)) continue;
-                printf("PCI device %x:%x.%x: %x:%x\n", bus, device, func, dev_info.vendor_id, dev_info.device_id);
-                pci_register_device(bus, device, func, &dev_info);
+                if(dev_info.header_type == PCI_TYPE_PCI_PCI) {
+                    // Schedule the bus for scanning
+                    int new_bus = dev_info.pci_bridge.secondary_bus_num;
+                    printf("PCI bus %x:%x.%x: bus %x\n", bus, device, func, new_bus);
+                    buses[new_bus / 8] |= (1 << (new_bus % 8));
+                } else {
+                    printf("PCI device %x:%x.%x: %x:%x\n", bus, device, func, dev_info.vendor_id, dev_info.device_id);
+                    pci_register_device(bus, device, func, &dev_info);
+                }
             }
             if(func == 0) continue;
         }
--- student-distrib/devices/pci.h
+++ student-distrib/devices/pci.h
@@ -6,13 +6,16 @@
 #define PCI_REG_ADDR 0xCF8
 #define PCI_REG_DATA 0xCFC
 
-// #define PCI_COUNT_BUS 255
-#define PCI_COUNT_BUS 8
+#define PCI_COUNT_BUS 255
 #define PCI_COUNT_DEVICE 32
 #define PCI_COUNT_FUNC 8
 
 #define PCI_MASK_BAR_MEMSPACE 0xfffffff0
 #define PCI_MASK_BAR_IOSPACE 0xfffffffc
+
+#define PCI_TYPE_STANDARD 0
+#define PCI_TYPE_PCI_PCI 1
+#define PCI_TYPE_CARDBUS 2
 
 typedef union {
     uint32_t val;
@@ -28,8 +31,9 @@ typedef union {
 } pci_addr_t;
 
 typedef union {
-    uint32_t val[16];
+    uint32_t val[18];
     struct {
+        // Basic information, shared among all 3 types.
         uint16_t vendor_id;
         uint16_t device_id;
         uint16_t command;
@@ -42,17 +46,72 @@ typedef union {
         uint8_t latency_timer;
         uint8_t header_type;
         uint8_t bist;
-        uint32_t bar[6];
-        uint32_t cardbus_cis_ptr;
-        uint16_t subsystem_vendor_id;
-        uint16_t subsystem_id;
-        uint32_t expansion_rom_base_addr;
-        uint8_t capabilities_pointer;
-        uint8_t reserved[7];
-        uint8_t interrupt_line;
-        uint8_t interrupt_pin;
-        uint8_t min_grant;
-        uint8_t max_latency;
+        union {
+            // For ordinary devices
+            struct {
+                uint32_t bar[6];
+                uint32_t cardbus_cis_ptr;
+                uint16_t subsystem_vendor_id;
+                uint16_t subsystem_id;
+                uint32_t expansion_rom_base_addr;
+                uint8_t capabilities_pointer;
+                uint8_t reserved[7];
+                uint8_t interrupt_line;
+                uint8_t interrupt_pin;
+                uint8_t min_grant;
+                uint8_t max_latency;
+            } device;
+            // For PCI-PCI bridge
+            struct {
+                uint32_t bar[2];
+                uint8_t primary_bus_num;
+                uint8_t secondary_bus_num;
+                uint8_t subordinate_bus_num;
+                uint8_t secondary_latency_timer;
+                uint8_t io_base;
+                uint8_t io_limit;
+                uint16_t secondary_status;
+                uint16_t memory_base;
+                uint16_t memory_limit;
+                uint16_t prefetchable_memory_base;
+                uint16_t prefetchable_memory_limit;
+                uint32_t prefetchable_base_upper;
+                uint32_t prefetchable_limit_upper;
+                uint16_t io_base_upper;
+                uint16_t io_limit_upper;
+                uint8_t capability_pointer;
+                uint8_t reserved[3];
+                uint32_t expansion_rom_base_addr;
+                uint8_t interrupt_line;
+                uint8_t interrupt_pin;
+                uint16_t bridge_control;
+            } pci_bridge;
+            // For CardBus bridge
+            struct {
+                uint32_t cardbus_socket_base_addr;
+                uint8_t offset_capabilities_list;
+                uint8_t reserved;
+                uint16_t secondary_status;
+                uint8_t pci_bus_num;
+                uint8_t cardbus_bus_num;
+                uint8_t subordinate_bus_num;
+                uint8_t cardbus_latency_timer;
+                uint32_t mem_base_addr0;
+                uint32_t mem_limit0;
+                uint32_t mem_base_addr1;
+                uint32_t mem_limit1;
+                uint32_t io_base_addr0;
+                uint32_t io_limit0;
+                uint32_t io_base_addr1;
+                uint32_t io_limit1;
+                uint8_t interrupt_line;
+                uint8_t interrupt_pin;
+                uint16_t bridge_control;
+                uint16_t subsystem_device_id;
+                uint16_t subsystem_vendor_id;
+                uint32_t legacy_mode_base_addr;
+            } cardbus_bridge;
+        };
     };
 } pci_device_t;
 
